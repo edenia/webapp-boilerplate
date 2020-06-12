32a7bb920755ed8ff80f96039128845019a3a1de
JustinCast <justincastillovalladares@gmail.com>
2020-06-11 19:36:58 -0600

    fix: fix indentation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
-import routes from './routes';
+import React from 'react'
+import PropTypes from 'prop-types'
+import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom'
+import routes from './routes'
 const App = ({ ual }) => (
-	<BrowserRouter>
-		<Switch>
-			{routes.map(({ path, component: Component }) => (
-				<Route key={`path-${path}`} path={path}>
-					<Component ual={ual} />
-				</Route>
-			))}
-			<Redirect exact from="/" to="/dashboard" />
-			<Redirect to="/not-found" />
-		</Switch>
-	</BrowserRouter>
-);
+  <BrowserRouter>
+    <Switch>
+      {routes.map(({ path, component: Component }) => (
+        <Route key={`path-${path}`} path={path}>
+          <Component ual={ual} />
+        </Route>
+      ))}
+      <Redirect exact from="/" to="/dashboard" />
+      <Redirect to="/not-found" />
+    </Switch>
+  </BrowserRouter>
+)
 
 App.propTypes = {
-	ual: PropTypes.object
-};
+  ual: PropTypes.object
+}
 
-export default App;
+export default App
